343404c8306bb85f56457476b51e4b443c2beecd
Dmitry Popov <dmitry.popov@godel.shellenergy.co.uk>
2024-03-13 20:39:41 +0100

    PROXY-7: Add logic to your app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import express from "express"
-import {config} from "./config/app_config.js";
-import {client} from "./client/meteor_nasa_client.js";
+import {config} from "./config/config.js";
+import {client} from "./client/nasa_client.js";
 
 
 export const app = express();
 
-app.get('/meteors', client);
+app.get('/meteors', (req, res) => {
+    let startDate = req.query.start_date;
+    let endDate = req.query.end_date;
+
+    if (!startDate) {
+        startDate = new Date().toISOString().split('T')[0];
+    }
+
+    if (!endDate) {
+        endDate = new Date().toISOString().split('T')[0];
+    }
+
+    client(req, res, startDate, endDate);
+});
 
 const port = config.appServerConfig.port;
 app.listen(port, () => {
