908ad32aa0d06e5cd2b8cdf9611cc29301d454ff
Dmitry Popov <dmitry.popov@godel.shellenergy.co.uk>
2024-03-15 11:52:24 +0100

    PROXY-9: add MeteorRequestDto

--- src/controller/meteor_controller.js
+++ src/controller/meteor_controller.js
@@ -1,11 +1,10 @@
-const {getNasaMeteorsData} = require('../client/nasa_meteor_client.js');
-const {getDate} = require('../util/date_util.js');
+const MeteorsRequestDto = require('../dto/meteor_request_dto');
+const {getNasaMeteorsData} = require('../service/nasa_meteor_service.js');
 
 const getMeteors = async (req, res, next) => {
     try {
-        const startDate = getDate().monday;
-        const endDate = getDate().friday;
-        const meteors = await getNasaMeteorsData(req, res, startDate, endDate);
+        const { startDate, endDate, countOnly, wereDangerous } = new MeteorsRequestDto(req.query);
+        const meteors = await getNasaMeteorsData(startDate, endDate);
         res.json(meteors);
     } catch (error) {
         next(error);
--- src/service/nasa_meteor_service.js
+++ src/service/nasa_meteor_service.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 const config = require('../config/env_config');
+const {getDate} = require('../util/date_util.js');
 const {mapMeteors} = require('../mapper/nasa_meteor_mapper');
 
-const getNasaMeteorsData = (req, res, startDate, endDate) => {
+const getNasaMeteorsData = async (startDate, endDate) => {
+    const requestStartDate = startDate ? startDate : getDate().monday;
+    const requestEndDate = endDate ? endDate : getDate().friday;
     return axios
         .get(config.nasaApiConfig.url, {
             params: {
-                start_date: startDate,
-                end_date: endDate,
+                start_date: requestStartDate,
+                end_date: requestEndDate,
                 api_key: config.nasaApiConfig.key,
             },
         })
@@ -17,6 +20,6 @@ const getNasaMeteorsData = (req, res, startDate, endDate) => {
         .catch((err) => {
             throw new Error(err.message);
         });
-};
+}
 
 module.exports = {getNasaMeteorsData};
