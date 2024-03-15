c02000c285ddee7f36b2c4858afec2174ab496d9
Dmitry Popov <dmitry.popov@godel.shellenergy.co.uk>
2024-03-15 12:24:59 +0100

    PROXY-9: count meteors

--- src/controller/meteor_controller.js
+++ src/controller/meteor_controller.js
@@ -4,7 +4,7 @@ const {getNasaMeteorsData} = require('../service/nasa_meteor_service.js');
 const getMeteors = async (req, res, next) => {
     try {
         const { startDate, endDate, countOnly, wereDangerous } = new MeteorsRequestDto(req.query);
-        const meteors = await getNasaMeteorsData(startDate, endDate);
+        const meteors = await getNasaMeteorsData(startDate, endDate, countOnly);
         res.json(meteors);
     } catch (error) {
         next(error);
--- src/service/nasa_meteor_service.js
+++ src/service/nasa_meteor_service.js
@@ -3,9 +3,12 @@ const config = require('../config/env_config');
 const {getDate} = require('../util/date_util.js');
 const {mapMeteors} = require('../mapper/nasa_meteor_mapper');
 
-const getNasaMeteorsData = async (startDate, endDate) => {
+const getNasaMeteorsData = async (startDate, endDate, countOnly) => {
     const requestStartDate = startDate ? startDate : getDate().monday;
     const requestEndDate = endDate ? endDate : getDate().friday;
+    console.log(`START DATE ${requestStartDate}`);
+    console.log(`END DATE ${requestEndDate}`);
+    console.log(`COUNT ${countOnly}`);
     return axios
         .get(config.nasaApiConfig.url, {
             params: {
@@ -14,12 +17,25 @@ const getNasaMeteorsData = async (startDate, endDate) => {
                 api_key: config.nasaApiConfig.key,
             },
         })
-        .then((response) =>
-            mapMeteors(response.data)
+        .then((response) => {
+                if (countOnly) {
+                    return {count: countMeteors(response.data)};
+                }
+
+                return mapMeteors(response.data);
+            }
         )
         .catch((err) => {
             throw new Error(err.message);
         });
 }
+
+const countMeteors = (nasaMeteors) => {
+    let totalCount = 0;
+    for (const date in nasaMeteors.near_earth_objects) {
+        totalCount += nasaMeteors.near_earth_objects[date].length;
+    }
+    return totalCount;
+};
 
 module.exports = {getNasaMeteorsData};
