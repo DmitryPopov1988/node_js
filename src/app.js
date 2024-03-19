938045db9f4e058eca531753e7fbd9dbcc47af1f
Dmitry Popov <dmitry.popov@godel.shellenergy.co.uk>
2024-03-19 11:38:09 +0100

    PROXY-13: add middleware exception handlers

--- src/app.js
+++ src/app.js
@@ -1,30 +1,26 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
 const config = require('./config/env_config');
 const meteorRouter = require('./router/meteor_router.js');
 const roverPhotoRouter = require('./router/rover_photo_router.js');
 const validator = require('./middleware/validator');
+const {exceptionHandler, pageNotFoundHandler} = require(`./middleware/exception`);
 
 const app = express();
+
+nunjucks.configure(path.resolve(__dirname, './template'), {
+    autoescape: true,
+    express: app,
+});
 
 app.use(express.json());
 
 app.use('/meteors', validator('meteorRequest', 'query'), meteorRouter);
 app.use('/photos', validator('userRequest', 'body'), roverPhotoRouter);
 
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode).json({
-        status: statusCode,
-        message: err.message
-    });
-});
-
-app.use('*', (req, res) =>
-    res.status(404).json({
-        status: 404,
-        message: 'The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.'
-    }),
-);
+app.use(exceptionHandler);
+app.use('*', pageNotFoundHandler);
 
 const port = config.appServerConfig.port;
 app.listen(port, () => {
